3e3cab808b597a90875b685a202becb9ac543b0e
Nicholas Landriault <nicholas.landriault@gmail.com>
2016-02-09 12:16:45 -0500

    Changed how the app connects (uses an argument instead of hardcoded)

--- src/applications/L2_LearningAPP.java
+++ src/applications/L2_LearningAPP.java
@@ -29,7 +29,7 @@ public class L2_LearningAPP {
 	public static void main(String[] args) {
 			
 		String serverURL = "rmi://127.0.0.1:1099/"; //this is the registry entry we want to use
-		String controllerExt = "controller";
+		String controllerExt = args[0];
 		//serverIntf is the interface we use to invoke methods on the server. Here we are retrieving the server object by looking it up in the registry
 		try {
 			SwitchHandlerAPI controllerIntf =(SwitchHandlerAPI)Naming.lookup(serverURL + controllerExt);
