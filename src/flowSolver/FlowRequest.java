5d1089e1332fca058a603f05faed976bdb0d4705
Shrammer1 <nicholas.landriault@hotmail.com>
2016-02-12 15:18:07 -0500

    Renamed flowSolver to flowsolver. Implemented Serializable where needed.

--- src/flowSolver/FlowRequest.java
+++
@@ -1,9 +0,0 @@
-package flowSolver;
-
-public class FlowRequest {
-	private Domain src;
-	private Domain dst;
-	private TrafficClass tClass;
-	private int priority;
-	private FlowAction fAction;
-}
--- src/flowsolver/FlowEntry.java
+++ src/flowsolver/FlowEntry.java
@@ -1,10 +1,11 @@
-package flowSolver;
+package flowsolver;
 
 import java.util.ArrayList;
 import org.openflow.protocol.OFFlowMod;
 import controller.OFSwitch;
 
-public class FlowEntry {
+public class FlowEntry{
+	
 	private OFFlowMod fmod;
 	private boolean active;
 	private OFSwitch Switch;
