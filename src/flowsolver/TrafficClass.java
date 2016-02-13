526c2a68a56dd320b7902dd44bdf90effc9790ab
Shrammer1 <nicholas.landriault@hotmail.com>
2016-02-12 21:24:44 -0500

    Fixed a typo.

--- src/flowsolver/TrafficClass.java
+++ src/flowsolver/TrafficClass.java
@@ -6,7 +6,7 @@ public class TrafficClass implements Serializable{
 
 	private static final long serialVersionUID = -7272204048173591835L;
 	private int tcpPortSrc;
-	private int tvpPortDst;
+	private int tcPortDst;
 	private int udpPortSrc;
 	private int udpPortDst;
 
