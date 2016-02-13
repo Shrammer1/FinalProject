8fc4585d85a2ef5a9b946d6466dcf82fd48d248e
ramonda077 <gustavobaron@cmail.carleton.ca>
2016-02-12 21:26:18 -0500

    Getters and Setters added for flowsolver classes

--- src/flowsolver/Domain.java
+++ src/flowsolver/Domain.java
@@ -7,13 +7,35 @@ import java.util.ArrayList;
 public class Domain implements Serializable {
 
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -1827587933052636802L;
 	private ArrayList<Domain> subDomains = new ArrayList<Domain>();
 	private ArrayList<byte[]> networks = new ArrayList<byte[]>();
 	private ArrayList<byte[]> macList = new ArrayList<byte[]>();
+	
+	
+	public ArrayList<Domain> getSubDomains() {
+		return subDomains;
+	}
+	public void setSubDomains(ArrayList<Domain> subDomains) {
+		this.subDomains = subDomains;
+	}
+	public ArrayList<byte[]> getNetworks() {
+		return networks;
+	}
+	public void setNetworks(ArrayList<byte[]> networks) {
+		this.networks = networks;
+	}
+	public ArrayList<byte[]> getMacList() {
+		return macList;
+	}
+	public void setMacList(ArrayList<byte[]> macList) {
+		this.macList = macList;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
 	
 	//contains, equals, getAll net/macs/subdomains
 }
--- src/flowsolver/FlowRequest.java
+++ src/flowsolver/FlowRequest.java
@@ -10,4 +10,39 @@ public class FlowRequest implements Serializable{
 	private TrafficClass tClass;
 	private int priority;
 	private FlowAction fAction;
+	public Domain getSrc() {
+		return src;
+	}
+	public void setSrc(Domain src) {
+		this.src = src;
+	}
+	public Domain getDst() {
+		return dst;
+	}
+	public void setDst(Domain dst) {
+		this.dst = dst;
+	}
+	public TrafficClass gettClass() {
+		return tClass;
+	}
+	public void settClass(TrafficClass tClass) {
+		this.tClass = tClass;
+	}
+	public int getPriority() {
+		return priority;
+	}
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+	public FlowAction getfAction() {
+		return fAction;
+	}
+	public void setfAction(FlowAction fAction) {
+		this.fAction = fAction;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
 }
